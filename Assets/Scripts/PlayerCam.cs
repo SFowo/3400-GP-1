77a453545b3f3e15bda879443fad01a5e9075cdf
GoForGoldner <goldner.t@northeastern.edu>
2025-01-30 17:08:11 -0500

    Changed sensitivity to accurately work with OpenGL (pls don't touch 😭)

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -2,41 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCam : MonoBehaviour
-{
-    public float sensX;
-    public float sensY;
+public class PlayerCam : MonoBehaviour {
+    public float sensX = 100f;
+    public float sensY = 100f;
 
     public Transform orientation;
 
     float xRotation;
     float yRotation;
 
-    private void Start()
-    {
+    private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    private void Update()
-    {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+    private void Update() {
+        if (Cursor.lockState != CursorLockMode.Locked)
+            Cursor.lockState = CursorLockMode.Locked;
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY;
 
         yRotation += mouseX;
-
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
-    public void StartPlayerDeath()
-    {
+    public void StartPlayerDeath() {
         sensX = 0f;
         sensY = 0f;
         xRotation = -40f;
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }
